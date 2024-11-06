67d9ab22e4631adda8c2ae57478ac1f353de182d
Bhasakr Roy <i.bhskr@hotmail.com>
2024-11-06 21:53:11 +0530

    fixed bugs

--- Server/app.js
+++
@@ -1,9 +0,0 @@
-// db.products.aggregate([
-//   { $match: { price: { $gt: 1200 } } },
-//   {
-//     $group: {
-//       _id: "$price",
-//       allColors: { $push: "$colors" },
-//     },
-//   },
-// ]);
