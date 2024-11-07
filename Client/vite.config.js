67a3adf515f6a2310bef36f07e629fdd08261e46
Bhaskar Roy <112053263+ibhskr@users.noreply.github.com>
2024-11-08 01:25:28 +0530

    Update vite.config.js
    update backend url

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000/",
+        target: "https://xclone-server-aqc1.onrender.com",
         // changeOrigin: true,
       },
     },
